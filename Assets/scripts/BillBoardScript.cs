13c487757ee71a77b035438d6f44a39b76425c2d
Ellis <563319@escg.ac.uk>
2026-03-09 11:12:28 +0000

    update3
    billboard script change

--- Assets/scripts/BillBoardScript.cs
+++ Assets/scripts/BillBoardScript.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 public class BillBoardScript : MonoBehaviour
 {
     private Transform cameraTransform;
-    
+    private void Awake()
+    {
+        
+
+    }
     void Start()
     {
         cameraTransform = Camera.main.transform;
+
     }
 
     
+
+
     void Update()
     {
-        transform.LookAt(cameraTransform);
+
+        transform.LookAt(cameraTransform, Vector3.up);
         transform.Rotate(0f, 180f, 0f);
+
     }
 }
