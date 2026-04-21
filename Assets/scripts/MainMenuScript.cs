bff2a6658292cdf4db5bff98dfdb8915ecfdbff4
Ellis <563319@escg.ac.uk>
2026-04-21 11:05:47 +0100

    update48
    ui + controller and gameplay = controller fully working

--- Assets/scripts/MainMenuScript.cs
+++ Assets/scripts/MainMenuScript.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public GameObject newButtonOptions;
+    public GameObject newButtonQuit;
+    public GameObject newButtonHelp;
+    public GameObject newButtonMain;
     public void playGame()
     {
         SceneManager.LoadSceneAsync(1);
@@ -10,5 +15,27 @@ public class MainMenuScript : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    
+    public void ReassignOptions()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(newButtonOptions);
+    }
+    public void ReassignQuit()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(newButtonQuit);
+    }
+    public void ReassignHelp()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(newButtonHelp);
+    }
+    public void ReassignMain()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(newButtonMain);
     }
 }
