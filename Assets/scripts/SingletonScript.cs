33ea80c5b4b96529c33e69f2eb3472df7d8f4420
Ellis <563319@escg.ac.uk>
2026-05-18 15:50:40 +0100

    update83

--- Assets/scripts/SingletonScript.cs
+++ Assets/scripts/SingletonScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class SingletonScript : MonoBehaviour
 {
-    public float mouseSensitivity = 800f;
+    public float mouseSensitivity = 300f;
 
     public static SingletonScript instance;
     ///plr stuff
